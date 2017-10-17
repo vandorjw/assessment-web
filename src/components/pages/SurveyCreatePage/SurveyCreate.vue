82d486c379a112a532c50f05bccd845adc8fb6bb
Joost van Dorp <joostvandorp@gmail.com>
2017-10-17 15:16:50 -0400

    Create survey in multiple languages

--- src/components/pages/SurveyCreatePage/SurveyCreate.vue
+++ src/components/pages/SurveyCreatePage/SurveyCreate.vue
@@ -2,14 +2,51 @@
   <div>
     <div v-if="this.$store.state.authenticated==true">
       <b-form v-on:submit.prevent="onSubmit">
-        <b-form-group id="nameInputGroup" label="Survey Name:" label-for="name" description="The name of the survey.">
-          <b-form-input id="nameInput" type="text" name="name" v-model="survey.translations.en.name" required>
-          </b-form-input>
-        </b-form-group>
-        <b-form-group id="descriptionInputGroup" label="Survey Description:" label-for="description">
-          <b-form-input id="descriptionInput" type="text" name="description" v-model="survey.translations.en.description" required>
-          </b-form-input>
-        </b-form-group>
+
+        <b-card no-body>
+          <b-tabs card no-fade>
+            <b-tab title="English" active>
+              <b-form-group id="englishNameInputGroup" label="English Survey Name:" label-for="en-name" description="The name of the survey in english.">
+                <b-form-input id="englishNameInput" type="text" name="en-name" v-model="survey.translations.en.name" required>
+                </b-form-input>
+              </b-form-group>
+              <b-form-group id="englishDescriptionInputGroup" label="English Survey Description:" label-for="en-description">
+                <b-form-input id="englishDescriptionInput" type="text" name="en-description" v-model="survey.translations.en.description" required>
+                </b-form-input>
+              </b-form-group>
+            </b-tab>
+            <b-tab title="French" >
+              <b-form-group id="frenchNameInputGroup" label="French Survey Name:" label-for="fr-name" description="The name of the survey in french.">
+                <b-form-input id="frenchNameInput" type="text" name="fr-name" v-model="survey.translations.fr.name">
+                </b-form-input>
+              </b-form-group>
+              <b-form-group id="frenchDescriptionInputGroup" label="French Survey Description:" label-for="fr-description">
+                <b-form-input id="frenchDescriptionInput" type="text" name="fr-description" v-model="survey.translations.fr.description">
+                </b-form-input>
+              </b-form-group>
+            </b-tab>
+            <b-tab title="Dutch" >
+              <b-form-group id="dutchNameInputGroup" label="Dutch Survey Name:" label-for="nl-name" description="The name of the survey in dutch.">
+                <b-form-input id="dutchNameInput" type="text" name="nl-name" v-model="survey.translations.nl.name">
+                </b-form-input>
+              </b-form-group>
+              <b-form-group id="dutchDescriptionInputGroup" label="Dutch Survey Description:" label-for="nl-description">
+                <b-form-input id="dutchDescriptionInput" type="text" name="nl-description" v-model="survey.translations.nl.description">
+                </b-form-input>
+              </b-form-group>
+            </b-tab>
+            <b-tab title="German" >
+              <b-form-group id="germanNameInputGroup" label="German Survey Name:" label-for="de-name" description="The name of the survey in german.">
+                <b-form-input id="germanNameInput" type="text" name="de-name" v-model="survey.translations.de.name">
+                </b-form-input>
+              </b-form-group>
+              <b-form-group id="germanDescriptionInputGroup" label="German Survey Description:" label-for="de-description">
+                <b-form-input id="germanDescriptionInput" type="text" name="de-description" v-model="survey.translations.de.description">
+                </b-form-input>
+              </b-form-group>
+            </b-tab>
+          </b-tabs>
+        </b-card>
 
         <b-form-group id="is_activeInputGroup" label="is_active:" label-for="is_active">
           <b-form-checkbox id="is_activeInput" name="is_active" v-model="survey.is_active">
@@ -47,6 +84,18 @@ export default {
       survey: {
         translations: {
           en: {
+            name: '',
+            description: ''
+          },
+          nl: {
+            name: '',
+            description: ''
+          },
+          fr: {
+            name: '',
+            description: ''
+          },
+          de: {
             name: '',
             description: ''
           }
