aa2a8ab00930a31d2e77eb8a0635b7ff48eba2af
Joost VanDorp <joostvandorp@gmail.com>
2018-01-01 22:13:43 -0500

    prepare for various question types

--- src/components/blocks/QuestionEditBlock/QuestionEdit.vue
+++ src/components/blocks/QuestionEditBlock/QuestionEdit.vue
@@ -2,14 +2,43 @@
   <div>
     <b-form v-on:submit.prevent="onSubmit">
 
-      <b-form-group id="questionInputGroup" label="Question:" label-for="question">
-        <b-form-input id="questionInput" type="text" name="question" v-model="question.translations.en.question" required>
-        </b-form-input>
-      </b-form-group>
+      <b-card no-body>
+        <b-tabs card no-fade>
+          <b-tab title="English" active>
+            <b-form-group label="Question:" label-for="en-question">
+              <b-form-input type="text" name="en-question" v-model="question.translations.en.question">
+              </b-form-input>
+            </b-form-group>
+          </b-tab>
+          <b-tab title="French">
+            <b-form-group label="Question:" label-for="fr-question">
+              <b-form-input type="text" name="fr-question" v-model="question.translations.fr.question">
+              </b-form-input>
+            </b-form-group>
+          </b-tab>
+          <b-tab title="Dutch">
+            <b-form-group label="Question:" label-for="nl-question">
+              <b-form-input type="text" name="nl-question" v-model="question.translations.nl.question">
+              </b-form-input>
+            </b-form-group>
+          </b-tab>
+          <b-tab title="German">
+            <b-form-group label="Question:" label-for="de-question">
+              <b-form-input type="text" name="de-question" v-model="question.translations.de.question">
+              </b-form-input>
+            </b-form-group>
+          </b-tab>
+        </b-tabs>
+      </b-card>
 
       <b-form-group id="is_requiredInputGroup" label="is_required:" label-for="is_required">
         <b-form-checkbox id="is_requiredInput" name="is_required" v-model="question.is_required">
         </b-form-checkbox>
+      </b-form-group>
+
+      <b-form-group label="of_type:" label-for="of_type">
+        <b-form-select v-model="question.of_type" :options="options" name="of_type">
+        </b-form-select>
       </b-form-group>
 
       <b-button type="submit" variant="primary">Submit</b-button>
@@ -31,12 +60,27 @@ export default {
         survey: this.survey_id,
         translations: {
           en: {
-            question: ''
+            question: null
+          },
+          nl: {
+            question: null
+          },
+          fr: {
+            question: null
+          },
+          de: {
+            question: null
           }
         },
         is_required: true,
-        of_type: 'txt'
-      }
+        of_type: null
+      },
+      options: [
+        { value: null, text: 'Please select an option' },
+        { value: 'tf', text: 'True/False' },
+        { value: 'mc', text: 'Multiple Choice' },
+        { value: 'txt', text: 'Text' }
+      ]
     }
   },
   methods: {
