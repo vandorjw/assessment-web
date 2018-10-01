2aea8d142a05ec9df02691c10286a50b007e02d7
Joost Van Dorp <joostvandorp@gmail.com>
2018-10-01 16:11:33 -0400

    update welcome page

--- src/components/pages/HelloPage/Hello.vue
+++ src/components/pages/HelloPage/Hello.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <p>
+    This is still a work in progress, but feel free to sign up and click around.
+    The vision for this site is to make it easy to create questions that other people can answer.
+    </p>
+    <p>
+    The source code is available <a href="https://github.com/vandorjw/django-assessment">here for the backend</a> and <a href="https://github.com/vandorjw/assessment-web">here for frontend</a>
+    </p>
+
   </div>
 </template>
 
@@ -25,7 +17,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Assessment App'
+      msg: 'Welcome to another survey App'
     }
   }
 }
